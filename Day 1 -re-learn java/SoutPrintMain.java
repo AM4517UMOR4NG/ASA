acd117b1a6268abf939c49715c9c991a5a62fad8
AM4517UMOR4NG <aekmohop@gmail.com>
2025-06-28 19:26:27 +0700

    Push filepertama re-larn java

---
+++ Day 1 -re-learn java/SoutPrintMain.java
@@ -0,0 +1,10 @@
+package Day 1 -re-learn java;
+
+public class SoutPrintMain {
+    public static void main(String[] args) {
+        //Cetak Kata dasar
+        System.out.println("Hello Dunia")
+    }
+    
+}
+ 
