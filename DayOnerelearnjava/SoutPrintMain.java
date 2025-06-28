b7fdff7cde20b6eba831cfcda1007b3a0ba0e5a9
AM4517UMOR4NG <aekmohop@gmail.com>
2025-06-28 19:31:52 +0700

    Re-name-package

---
+++ DayOnerelearnjava/SoutPrintMain.java
@@ -0,0 +1,11 @@
+package DayOnerelearnjava;
+
+public class SoutPrintMain {
+    public static void main(String[] args) {
+        //Cetak Kata dasar
+        System.out.println("Hello Dunia");
+        System.out.println("Kembali ke dasar java");
+    }
+    
+}
+ 
