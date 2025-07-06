36e36da67847db01634787fb89304777719422bb
AM4517UMOR4NG <aekmohop@gmail.com>
2025-07-06 21:58:26 +0700

    push  ifelse

---
+++ DayTworelearnjava/IfElse.java
@@ -0,0 +1,19 @@
+package DayTworelearnjava;
+
+public class IfElse {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int nilai = 75;
+
+        // Struktur if-else
+        if (nilai >= 80) {
+            System.out.println("Nilai Anda A");
+        } else if (nilai >= 70) {
+            System.out.println("Nilai Anda B");
+        } else if (nilai >= 60) {
+            System.out.println("Nilai Anda C");
+        } else {
+            System.out.println("Nilai Anda D");
+        }
+    }
+}
